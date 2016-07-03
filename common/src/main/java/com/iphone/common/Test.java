2135d7d58c5c5db8560ee94f72a25419f09fe77d
YangZigang <tianshanxuester@gmail.com>
2016-07-03 10:23:38 +0800

    init

---
+++ common/src/main/java/com/iphone/common/Test.java
@@ -0,0 +1,10 @@
+package com.ingphone.common;
+
+/**
+ * Created by yangzigang on 16/7/2.
+ */
+public class Test {
+    public static void test() {
+        System.out.println("hello test");
+    }
+}
---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyClass {
+    static public void test() {
+        System.out.println("hello from lib");
+    }
+}
